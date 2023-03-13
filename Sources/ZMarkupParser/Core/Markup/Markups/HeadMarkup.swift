6c5c8c2c383a825b107a417f48c7e670cd6a7fa9
zhgchgli <zhgchgli@gmail.com>
2023-03-13 20:08:06 +0800

    [Feat] add HeadMarkup

--- Sources/ZMarkupParser/Core/Markup/Markups/HeadMarkup.swift
+++ Sources/ZMarkupParser/Core/Markup/Markups/HeadMarkup.swift
@@ -1,8 +1,31 @@
 //
-//  File.swift
+//  HeadMarkup.swift
 //  
 //
-//  Created by zhgchgli on 2023/3/13.
+//  Created by https://zhgchg.li on 2023/3/13.
 //
 
 import Foundation
+
+final class HeadMarkup: Markup {
+    enum Level {
+        case h1
+        case h2
+        case h3
+        case h4
+        case h5
+        case h6
+    }
+    
+    let levle: Level
+    init(levle: Level) {
+        self.levle = levle
+    }
+    
+    weak var parentMarkup: Markup? = nil
+    var childMarkups: [Markup] = []
+    
+    func accept<V>(_ visitor: V) -> V.Result where V : MarkupVisitor {
+        return visitor.visit(self)
+    }
+}
